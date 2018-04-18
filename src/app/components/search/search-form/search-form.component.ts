2bcaded1ad7ed20b83a69283fb32c0bbefa97297
Gabriel Lima <naive505@gmail.com>
2018-04-18 04:12:46 +0100

    Component: Add debounce Time on searching

--- src/app/components/search/search-form/search-form.component.ts
+++ src/app/components/search/search-form/search-form.component.ts
@@ -1,37 +1,39 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { User } from '../../../shared/models/user.model';
 import { GithubAPIService } from '../../../shared/services/githubAPI.service';
-import { TransitiveCompileNgModuleMetadata } from '@angular/compiler';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
   styleUrls: ['./search-form.component.scss']
 })
-export class SearchFormComponent {
-  public loading: Boolean;
-  public userName: string;
+export class SearchFormComponent implements OnInit {
+  private searchTerms = new Subject<string>();
 
-  constructor(private githubAPIService: GithubAPIService) {}
+  constructor(private githubAPIService: GithubAPIService) { }
 
-  onUpdateUserName(event: Event) {
-    // .pipe(
-    //   debounceTime(1000),
-    //   distinctUntilChanged()),
-    //   switchMap(inputVal => this.getUsers(inputVal))
-    // )
-    this.userName = (<HTMLInputElement>event.target).value;
-    this.githubAPIService.loading.emit(true);
-    this.getUsers(this.userName);
+  ngOnInit() {
+    this.searchTerms.pipe(
+        debounceTime(1000),
+        distinctUntilChanged(),
+      )
+      .subscribe(term => this.getUsers(term)
+    );
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
   getUsers(userName: string) {
     this.githubAPIService.getUsers(userName).subscribe(
-      (users: User[]) => {
-        this.githubAPIService.userNameUpdated.emit(users);
-        this.githubAPIService.loading.emit(false);
-      }
-    );
+        (users: User[]) => {
+          this.githubAPIService.userNameUpdated.emit(users);
+          this.githubAPIService.loading.emit(false);
+        }
+      );
   }
 }
